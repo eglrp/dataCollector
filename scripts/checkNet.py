0f31b7f0831299fa8bdbf70f4641758d792b8fa6
jianping <lijianping@whu.edu.cn>
2017-10-10 23:14:16 +0800

    fix a bug

--- scripts/checkNet.py
+++ scripts/checkNet.py
@@ -3,4 +3,4 @@ import time
 while(1):
     time.sleep(30)
     os.system("ifconfig eth0 192.168.1.255 netmask 255.255.255.0")
-    os.systme("route add 192.168.1.201 eth0")
+    os.system("route add 192.168.1.201 eth0")
