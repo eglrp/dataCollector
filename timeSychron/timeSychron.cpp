b1fb100642e43ce51ba21233d2beb12070d7804c
jianping <lijianping@whu.edu.cn>
2017-10-10 23:06:54 +0800

    delete nmea message

--- timeSychron/timeSychron.cpp
+++ timeSychron/timeSychron.cpp
@@ -65,7 +65,7 @@ int main()
                 gettimeofday(&tv,NULL);
                 ofsIMUTime<<tv.tv_sec<<","<<tv.tv_usec<<"\n";
             }
-            if(imuCount == 20)
+            if(imuCount == 40)
             {
                 mutexSetCameraGPIO.lock();
                 bSetHighGPIOCamera = true;
@@ -160,54 +160,54 @@ int main()
 
 
     // 2. a thread for output GPIO and NMEA
-    const std::string port_ttyS4 = "/dev/ttyS4";
-    const unsigned int baud_nmea = 9600;
-    serial::Serial serial_nmea(port_ttyS4, baud_nmea, serial::Timeout::simpleTimeout(1000));
-    if (!serial_nmea.isOpen())
-    {
-        std::cout<<"can not open serial "<<port_ttyS4<<std::endl;
-        return 0;
-    }
-
-
-    std::function<void(void)> outPutGPIOFunction = [&](void)->void {
-        while(!bStop)
-        {
-            if(bSetHighGPIO)
-            {
-                //set high gpio
-                //std::cout<<"set high gpio"<<std::endl;
-                GPIO::gpio_set_value(goio_pps,GPIO::Pin::HIGH);
-                //wait for 10 us to guarantee the GPIO is set
-                std::this_thread::sleep_for(std::chrono::microseconds(1000));
-                //send NMEA message
-                timeval tv;
-                gettimeofday(&tv,NULL);
-                tm time = *localtime(&tv.tv_sec);
-                //std::cout<<time.tm_sec<<": send NMEA message"<<std::endl;
-                //std::cout<<forgeNMEAMeaage(time)<<std::endl;
-                serial_nmea.write(forgeNMEAMeaage(time));
-
-                mutexHighGPIO.lock();
-                bSetHighGPIO = false;
-                mutexHighGPIO.unlock();
-            }
-            if(bSetLowGPIO)
-            {
-                //set low gpio
-                //std::cout<<"set low"<<std::endl;
-                GPIO::gpio_set_value(goio_pps,GPIO::Pin::LOW);
-                mutexHighGPIO.lock();
-                bSetLowGPIO = false;
-                mutexHighGPIO.unlock();
-            }
-        }
-
-    };
-    std::thread outPutGPIOThread(outPutGPIOFunction);
+//    const std::string port_ttyS4 = "/dev/ttyS4";
+//    const unsigned int baud_nmea = 9600;
+//    serial::Serial serial_nmea(port_ttyS4, baud_nmea, serial::Timeout::simpleTimeout(1000));
+//    if (!serial_nmea.isOpen())
+//    {
+//        std::cout<<"can not open serial "<<port_ttyS4<<std::endl;
+//        return 0;
+//    }
+//
+//
+//    std::function<void(void)> outPutGPIOFunction = [&](void)->void {
+//        while(!bStop)
+//        {
+//            if(bSetHighGPIO)
+//            {
+//                //set high gpio
+//                //std::cout<<"set high gpio"<<std::endl;
+//                GPIO::gpio_set_value(goio_pps,GPIO::Pin::HIGH);
+//                //wait for 10 us to guarantee the GPIO is set
+//                std::this_thread::sleep_for(std::chrono::microseconds(1000));
+//                //send NMEA message
+//                timeval tv;
+//                gettimeofday(&tv,NULL);
+//                tm time = *localtime(&tv.tv_sec);
+//                //std::cout<<time.tm_sec<<": send NMEA message"<<std::endl;
+//                //std::cout<<forgeNMEAMeaage(time)<<std::endl;
+//                serial_nmea.write(forgeNMEAMeaage(time));
+//
+//                mutexHighGPIO.lock();
+//                bSetHighGPIO = false;
+//                mutexHighGPIO.unlock();
+//            }
+//            if(bSetLowGPIO)
+//            {
+//                //set low gpio
+//                //std::cout<<"set low"<<std::endl;
+//                GPIO::gpio_set_value(goio_pps,GPIO::Pin::LOW);
+//                mutexHighGPIO.lock();
+//                bSetLowGPIO = false;
+//                mutexHighGPIO.unlock();
+//            }
+//        }
+//
+//    };
+//    std::thread outPutGPIOThread(outPutGPIOFunction);
 
     timingThread.join();
-    outPutGPIOThread.join();
+//    outPutGPIOThread.join();
     //checkThread.join();
 
     std::cin.ignore(1024,'x');
